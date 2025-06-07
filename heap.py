17e9398ba43b4c69cb3915af34963de79ed72f45
Estela <estelasimoes108@gmail.com>
2025-06-06 21:54:20 -0300

    Foi criada as funções desalocar e alterar heap, além de que todo bloco alocado possui um nome para a identificação na hora de desalocar o mesmo

--- heap.py
+++ heap.py
@@ -1,104 +1,99 @@
 from colorama import init, Back, Style
 init(autoreset=True)
+
 class Livre:
     def __init__(self, endereco, tamanho):
         self.inicio = endereco
         self.tamanho = tamanho
 
 class Alocado:
-    def __init__(self, inicio, final):
+    def __init__(self, label, inicio, final):
+        self.label = label
         self.inicio = inicio
         self.final = final
 
-#Classe heap que é armazenado em um vetor que possui uma série de atributos.
 class Heap:
     def __init__(self, tamanho, tipo):
-            self.heap = [0] * tamanho 
-            self.livres = [Livre(0, tamanho)]
-            self.alocados = []
-            self.capacidade = tamanho
-            self.numLivres = 1
-            self.numAlocados = 0
-            self.ultimaPosicao = 0
-            self.tipo = tipo
+        self.heap = [0] * tamanho
+        self.livres = [Livre(0, tamanho)]
+        self.alocados = []
+        self.capacidade = tamanho
+        self.numLivres = 1
+        self.numAlocados = 0
+        self.ultimaPosicao = 0
+        self.tipo = tipo
 
     def encontrar_bloco_livre(self, tamanho):
         indiceEscolhido = -1
-        if self.tipo == "1":
-            menorTamanho = self.capacidade + 1
-            for i, bloco in enumerate(self.livres):
-                if bloco.tamanho >= tamanho and bloco.tamanho < menorTamanho:
-                    menorTamanho = bloco.tamanho
+        if self.tipo == "1":  # Best
+            menor = self.capacidade + 1
+            for i, b in enumerate(self.livres):
+                if b.tamanho >= tamanho and b.tamanho < menor:
+                    menor = b.tamanho
                     indiceEscolhido = i
-        elif self.tipo == "2":
-            maiorTamanho = -1
-            for i, bloco in enumerate(self.livres):
-                if bloco.tamanho >= tamanho and bloco.tamanho > maiorTamanho:
-                    maiorTamanho = bloco.tamanho
+        elif self.tipo == "2":  # Worst
+            maior = -1
+            for i, b in enumerate(self.livres):
+                if b.tamanho >= tamanho and b.tamanho > maior:
+                    maior = b.tamanho
                     indiceEscolhido = i
-        elif self.tipo == "3":
-            for i, bloco in enumerate(self.livres):
-                if bloco.tamanho >= tamanho:
+        elif self.tipo == "3":  # First
+            for i, b in enumerate(self.livres):
+                if b.tamanho >= tamanho:
                     indiceEscolhido = i
                     break
-        elif self.tipo == "4":
-            inicio = self.ultimaPosicao
-            for i in range(self.numLivres):
-                posicaoAtual = (inicio + i) % self.numLivres
-                bloco = self.livres[posicaoAtual]
-                if bloco.tamanho >= tamanho:
-                    indiceEscolhido = posicaoAtual
-                    self.ultimaPosicao = (posicaoAtual + 1) % self.numLivres
+        elif self.tipo == "4":  # Next
+            start = self.ultimaPosicao
+            for k in range(self.numLivres):
+                i = (start + k) % self.numLivres
+                b = self.livres[i]
+                if b.tamanho >= tamanho:
+                    indiceEscolhido = i
+                    self.ultimaPosicao = (i + 1) % self.numLivres
                     break
         return indiceEscolhido
 
-    def alocar(self, tamanho):
-        indice = self.encontrar_bloco_livre(tamanho)
-        if indice != -1:
-            bloco = self.livres[indice]
-            for i in range(bloco.inicio, bloco.inicio + tamanho):
-                self.heap[i] = 1
-            self.alocados.append(Alocado(bloco.inicio, bloco.inicio + tamanho - 1))
-            self.numAlocados += 1
-            if bloco.tamanho == tamanho:
-                del self.livres[indice]
-                self.numLivres -= 1
-            else:
-                bloco.inicio += tamanho
-                bloco.tamanho -= tamanho
-            print("Memória alocada com sucesso.")
+    def alocar(self, tamanho, label):
+        idx = self.encontrar_bloco_livre(tamanho)
+        if idx == -1:
+            print(f"Erro: não foi possível alocar {tamanho} blocos.")
+            return
+        b = self.livres[idx]
+        for i in range(b.inicio, b.inicio + tamanho):
+            self.heap[i] = 1
+        novo = Alocado(label, b.inicio, b.inicio + tamanho - 1)
+        self.alocados.append(novo)
+        self.numAlocados += 1
+        if b.tamanho == tamanho:
+            del self.livres[idx]
+            self.numLivres -= 1
         else:
-            print(f"Erro: Não foi possível alocar {tamanho} blocos.")
+            b.inicio += tamanho
+            b.tamanho -= tamanho
+        print("Memória alocada com sucesso.")
 
-    def desalocar(self, inicio, tamanho):
-        for i in range(inicio, inicio + tamanho):
-            self.heap[i] = 0
-        for i, bloco in enumerate(self.alocados):
-            if bloco.inicio == inicio:
+    def desalocar(self, label) -> bool:
+        for i, a in enumerate(self.alocados):
+            if a.label == label:
+                tamanho = a.final - a.inicio + 1
+                for j in range(a.inicio, a.inicio + tamanho):
+                    self.heap[j] = 0
                 del self.alocados[i]
                 self.numAlocados -= 1
-                break
-        self.livres.append(Livre(inicio, tamanho))
-        self.numLivres += 1
+                self.livres.append(Livre(a.inicio, tamanho))
+                self.numLivres += 1
+                return True
+        return False
 
     def exibe(self) -> None:
-
         print("\nEstado do Heap:")
-        print("Legenda: " + Back.RED + "[X]" + Style.RESET_ALL + " = Ocupado, " + Back.GREEN + "[ ]" + Style.RESET_ALL + " = Livre\n")
-
-        for bloco in self.heap:
-            if bloco:
-                 print(Back.RED + "[X]" + Style.RESET_ALL, end=" ")
+        print("Legenda: " + Back.RED + "[X]" + Style.RESET_ALL + " = Ocupado, "
+              + Back.GREEN + "[ ]" + Style.RESET_ALL + " = Livre\n")
+        for bit in self.heap:
+            if bit:
+                print(Back.RED + "[X]" + Style.RESET_ALL, end=" ")
             else:
-                 print(Back.GREEN + "[ ]" + Style.RESET_ALL, end=" ")
+                print(Back.GREEN + "[ ]" + Style.RESET_ALL, end=" ")
         print("\n\nBlocos Alocados:")
-
-        for i, bloco in enumerate(self.alocados, start=1):
-                print(f"Bloco {i}: Início = {bloco.inicio}, Final = {bloco.final}")
-
-      
-
-    
-        
-
-
+        for a in self.alocados:
+            print(f"- {a.label}: Início = {a.inicio}, Final = {a.final}")
--- main.py
+++ main.py
@@ -22,24 +22,27 @@ def exibir_heap(heaps: dict) -> None:
     if alvo in heaps:
         heaps[alvo].exibe()
     else:
-        print("\n Heap não encontrado.")
+        print("\nHeap não encontrado.")
     input("\nPressione <Enter> para continuar...")
 
 
 def criar_heap(heaps: dict) -> None:
-    
     limpar_tela()
     print("Antes de criar seu heap informe o tipo:\n\n")
-    print("1-Best Heap\n2-worst Heap\n3-First Heap\n4-Next Heap\n")
+    print("1 - Best Heap (menor bloco disponível)")
+    print("2 - Worst Heap (maior bloco disponível)")
+    print("3 - First Heap (primeiro bloco disponível)")
+    print("4 - Next Heap (aloca circularmente)")
 
-    tipo = (input("Digite da opção desejada: "))
-
-    nome, tamanho = input("Digite o nome do heap e o número de alocações, respectivamente: ").split()
+    tipo = input("\nDigite a opção desejada (1-4): ")
+    nome, tamanho = input("\nDigite o nome do heap e o número de alocações, respectivamente: ").split()
     tamanho = int(tamanho)
-    heaps[nome] = Heap(tamanho, tipo)  
-    
+    heaps[nome] = Heap(tamanho, tipo)
+    limpar_tela()
+    print(f"Heap '{nome}' criado com tipo {tipo} e capacidade {tamanho}.")
     heaps[nome].exibe()
-   
+    input("\nPressione <Enter> para continuar...")
+
 
 def alocar_heap(heaps: dict):
     limpar_tela()
@@ -55,22 +58,23 @@ def alocar_heap(heaps: dict):
 
     alvo = input("\nDigite o nome do heap para alocar: ")
     if alvo not in heaps:
-        print("\n Heap não encontrado.")
+        print("\nHeap não encontrado.")
         input("\nPressione <Enter> para continuar...")
         return
 
     try:
-        tamanho = int(input("Quantos blocos deseja alocar? "))
+        tamanho = int(input("\nQuantos blocos deseja alocar? "))
     except ValueError:
-        print("\n Valor inválido.")
+        print("\nValor inválido.")
         input("\nPressione <Enter> para continuar...")
         return
 
-    heaps[alvo].alocar(tamanho)
+    label = input("\nDigite um nome para esta alocação: ")
+    heaps[alvo].alocar(tamanho, label)
     input("\nPressione <Enter> para continuar...")
 
-def deletar_heap(heaps: dict) -> None:
 
+def desalocar_heap(heaps: dict) -> None:
     limpar_tela()
     if not heaps:
         print("\nNenhum heap criado ainda.")
@@ -82,20 +86,55 @@ def deletar_heap(heaps: dict) -> None:
     for nome in heaps:
         print(f"- {nome}")
 
-    alvo = input("\nDigite o nome do heap para alocar: ")
+    alvo = input("\nDigite o nome do heap para desalocar: ")
     if alvo not in heaps:
-        print("\n Heap não encontrado.")
+        print("\nHeap não encontrado.")
         input("\nPressione <Enter> para continuar...")
         return
-    
+
     limpar_tela()
-    del heaps[alvo]
-    print(f"\nHeap '{alvo}' removido com sucesso!")
+    
+    heap_obj = heaps[alvo]
+    if not heap_obj.alocados:
+        print("\nNenhuma alocação existente neste heap.")
+        input("\nPressione <Enter> para continuar...")
+        return
+    heaps[alvo].exibe()
+
+    nome_bloco = input("\nDigite o nome da alocação que deseja desalocar: ")
+    result = heap_obj.desalocar(nome_bloco)
+    if result:
+        print(f"\nAlocação '{nome_bloco}' desalocada com sucesso.")
+    else:
+        print(f"\nAlocação '{nome_bloco}' não encontrada.")
+    input("\nPressione <Enter> para continuar...")
+
+
+def alterar_heap(heaps: dict) -> None:
+    limpar_tela()
+    if not heaps:
+        print("\nNenhum heap criado ainda.")
+        input("\nPressione <Enter> para continuar...")
+        return
+
+    limpar_tela()
+    print("Heaps disponíveis:")
+    for nome in heaps:
+        print(f"- {nome}")
+
+    alvo = input("\nDigite o nome do heap para alterar tipo: ")
+    if alvo not in heaps:
+        print("\nHeap não encontrado.")
+        input("\nPressione <Enter> para continuar...")
+        return
+
+    novo_tipo = input("\nDigite o novo tipo (1-4): ")
+    heaps[alvo].tipo = novo_tipo
+    print(f"\nTipo do heap '{alvo}' alterado para {novo_tipo}.")
     input("\nPressione <Enter> para continuar...")
 
 
 def menu_principal():
-    
     heaps: dict[str, Heap] = {}
 
     while True:
@@ -105,8 +144,9 @@ def menu_principal():
         print("2 - Deletar Heap")
         print("3 - Exibir Heap")
         print("4 - Alocar")
-        print("5 - Atribuição")
-        print("6 - Sair\n")
+        print("5 - Alterar Tipo do Heap")
+        print("6 - Desalocar")
+        print("7 - Sair\n")
 
         opcao = input("Digite o número da opção desejada: ")
 
@@ -119,8 +159,10 @@ def menu_principal():
         elif opcao == "4":
             alocar_heap(heaps)
         elif opcao == "5":
-            atribuir_heap(heaps)
+            alterar_heap(heaps)
         elif opcao == "6":
+            desalocar_heap(heaps)
+        elif opcao == "7":
             limpar_tela()
             print("Saindo...\n")
             break
@@ -131,9 +173,3 @@ def menu_principal():
 
 if __name__ == "__main__":
     menu_principal()
-    
-
-
-
-
-
